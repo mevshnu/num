e7473e46452adb71f7d8aff77901a439ceb4354c
mevshnu <vishnusivan1000@gmail.com>
2022-11-21 12:37:58 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("enter the number");
-        Scanner sc = new Scanner(System.in);
         System.out.println(" enter 1 to find largest number among 3 ");
         System.out.println(" enter 2 to find smallest number among 3");
         System.out.println(" enter 3 to find the number is prime or not");
@@ -12,6 +11,26 @@ public class Main {
         System.out.println(" enter 5 to reverse a number");
         System.out.println(" enter 6 to exit");
     }
+void largest()
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter 3 number");
+    int x=sc.nextInt();
+    int y=sc.nextInt();
+    int z=sc.nextInt();
+    if (x>y && x>z)
+    {
+            System.out.println("largest number is"+x);
+    }
+    if (y>z && y>x)
+    {
+        System.out.println("largest number is"+y);
+    }
+    if (z>y && z>x)
+    {
+        System.out.println("largest number is"+y);
+    }
 
+}
 
 }
