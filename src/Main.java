ea32a0ab03b2fee269965140a7a9190a34d158ed
mevshnu <vishnusivan1000@gmail.com>
2022-11-21 12:28:20 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("enter the number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter 1 to find largest number among 3 ");
+        System.out.println(" enter 2 to find smallest number among 3");
+        System.out.println(" enter 3 to find the number is prime or not");
+        System.out.println(" enter 4 to find the number is even or not ");
+        System.out.println(" enter 5 to reverse a number");
+        System.out.println(" enter 6 to exit");
+    }
+
+
+}
